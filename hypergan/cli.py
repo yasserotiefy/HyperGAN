4feae7f25df2a2675aedf658f9bb5d77fcb59670
Mohamed Nasser <mhmeeaad@gmail.com>
2018-06-18 13:44:02 +0200

    Refactor[ cli.by ] Edit Samples Path at Line 100

--- hypergan/cli.py
+++ hypergan/cli.py
@@ -97,7 +97,7 @@ class CLI:
         self.gan.step()
 
         if(self.steps % self.sample_every == 0):
-            sample_file="samples/%06d.png" % (self.samples)
+            sample_file="/content/drive/GP/Hyper_GAN/samples/%06d.png" % (self.samples)
             self.create_path(sample_file)
             sample_list = self.sample(sample_file)
             if self.args.use_hc_io:
